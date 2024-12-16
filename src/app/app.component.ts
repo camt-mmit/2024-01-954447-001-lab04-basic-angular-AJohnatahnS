604fb0a70f972b637ddfc7b5474d3d5462981c3b
AJohnatahnS <95701398+AJohnatahnS@users.noreply.github.com>
2024-12-16 16:18:29 +0700

    ::add_all_need

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendComponent } from './friend/friend.component';
+import { StatusComponent } from './status/status.component';
 
 export const routes: Routes = [
   { path: 'hello-world', component: HelloWorldComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'friend', component: FriendComponent },
+  { path: 'status', component: StatusComponent },
 ];
